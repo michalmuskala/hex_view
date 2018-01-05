990ab49ef5b9aa32095f1c64fea08c6a6b8600a3
Joseph An <an.joseph891@gmail.com>
2018-01-04 23:25:23 -0500

    Fetch and display all packages from directory
    * Hexview.Package module for fetching package files
    * update PackageController.show/2 to render package files
    * placeholder packages within priv/packages/

--- lib/hexview_web/controllers/package_controller.ex
+++ lib/hexview_web/controllers/package_controller.ex
@@ -1,7 +1,10 @@
 defmodule HexviewWeb.PackageController do
   use HexviewWeb, :controller
 
+  alias Hexview.Package
+
   def show(conn, %{"name" => name}) do
-    render conn, "show.html", name: name
+    files = Package.list_files(name)
+    render conn, "show.html", name: name, files: files
   end
 end
