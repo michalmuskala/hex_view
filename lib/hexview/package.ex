981ef1f4c38ed12a4972fc955515c11df66abd2e
Joseph An <an.joseph891@gmail.com>
2018-01-05 01:23:05 -0500

    Render files, directories, and code

--- lib/hexview/package.ex
+++ lib/hexview/package.ex
@@ -2,6 +2,28 @@ defmodule Hexview.Package do
   @package_directory "priv/packages/"
 
   def list_files(package) do
-    Path.wildcard(@package_directory <> package <> "/**/*")
+    {:ok, files} = File.ls(path(package))
+    Enum.map(files, &to_map/1)
   end
+
+  def list_files(package, dir) do
+    {:ok, files} = File.ls(path(package, dir))
+    Enum.map(files, &to_map/1)
+  end
+
+  defp to_map(path) do
+    %{
+      name: path,
+      is_directory: File.dir?(path),
+    }
+  end
+
+  def fetch_file(package, dir) do
+    {:ok, binary} = File.read(path(package, dir))
+
+    %{name: Path.basename(dir), content: binary}
+  end
+
+  defp path(package), do: "#{@package_directory}/#{package}"
+  defp path(package, dir), do: "#{@package_directory}/#{package}/#{dir}"
 end
--- lib/hexview_web/controllers/package_controller.ex
+++ lib/hexview_web/controllers/package_controller.ex
@@ -7,4 +7,14 @@ defmodule HexviewWeb.PackageController do
     files = Package.list_files(name)
     render conn, "show.html", name: name, files: files
   end
+
+  def tree(conn, %{"name" => name, "path" => path}) do
+    files = Package.list_files(name, Enum.join(path, "/"))
+    render conn, "show.html", name: name, files: files
+  end
+
+  def blob(conn, %{"name" => name, "path" => path}) do
+    file = Package.fetch_file(name, Enum.join(path, "/"))
+    render conn, "blob.html", file: file
+  end
 end
